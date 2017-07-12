f2250d249b6dfe743808cbeb9abadf78afa4951f
ctam91 <ctam91@gmail.com>
2017-07-11 20:37:13 -0700

    add render template for add.html

--- main.py
+++ main.py
@@ -14,8 +14,23 @@ class Blog(db.Model):
     title = db.Column(db.String(120))
     body = db.Column(db.String(800))
 
-    def __init__(self, title):
+    def __init__(self, title, body):
         self.title = title
+        self.body = body 
+
+@app.route('/newpost', methods=['POST', 'GET'])
+def add_post():
+ 
+    if request.method == 'POST':
+        title_name = request.form['title']
+        body_name = request.form['body']
+        new_title = Blog(title_name, body_name)
+        db.session.add(new_title)
+        db.session.commit()
+
+    titles = Blog.query.all()
+    return render_template('add.html', titles=titles)
+
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -23,12 +38,13 @@ def index():
 
     if request.method == 'POST':
         title_name = request.form['title']
-        new_title = Blog(title_name)
+        body_name = request.form['body']
+        new_title = Blog(title_name, body_name)
         db.session.add(new_title)
         db.session.commit()
 
     titles = Blog.query.all()
-    return render_template('todos.html', titles=titles)
+    return render_template('blog.html', titles=titles)
 
 
 if __name__ == '__main__':
