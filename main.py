c81f19c346b545b7175436903f2a9adde862f3cb
ctam91 <ctam91@gmail.com>
2017-07-12 18:58:33 -0700

    finish requirements for build-a-blog

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blog@local
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
-
+#create a Blog class with id, title, and body properties
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
@@ -18,6 +18,7 @@ class Blog(db.Model):
         self.title = title
         self.body = body 
 
+#create /newpost route and renders add template. If user leaves title or body blank, then return errors. 
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
     if request.method == 'POST':
@@ -39,24 +40,23 @@ def newpost():
             new_post = Blog(title, body)
             db.session.add(new_post)
             db.session.commit()
-            titles = Blog.query.all()
-            return render_template('blog.html', titles=titles)
+            blog_id = str(new_post.id) #grab the id for the record you just created
+            return redirect('/blog?id='+blog_id)
         else: 
             return render_template('add.html', title_error=title_error, body_error=body_error, title=title, body=body)
         
     return render_template('add.html')
 
-
+#create /blog route to display blog
 @app.route('/blog', methods=['POST', 'GET'])
 def blog():
-
     #since this is a get request, use request.args.get to retrieve id of blog post
     blog_id = request.args.get('id')
 
     #if blog_id exists, send your db a query and find the post associated with that id. Render post.html with that post's title and blog
     if blog_id:
         post = Blog.query.filter_by(id=blog_id).first()
-        return render_template("post.html", post=post.title, body=post.body)
+        return render_template("post.html", title=post.title, body=post.body)
     
 # If there are no specific posts, show entire blog. 
     titles = Blog.query.all()
